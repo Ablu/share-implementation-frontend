bd1cdab894b3b98a187c98698c46829b4e8a4a5d
Erik Schilling <ablu.erikschilling@gmail.com>
2016-09-15 09:35:54 +0200

    Log messages received on client side

--- app/share.service.ts
+++ app/share.service.ts
@@ -7,11 +7,16 @@ export class ShareService {
 
     constructor() {
         this.websocket = new WebSocket('ws://localhost:9456/');
+        this.websocket.addEventListener('message', this.onMessage);
     }
 
     getStorageNodes(): Promise<StorageNode[]> {
         return new Promise<StorageNode[]>(resolve => {
             resolve([]);
         })
+    }
+
+    onMessage(message: MessageEvent) {
+        console.log(message);
     }
 }
