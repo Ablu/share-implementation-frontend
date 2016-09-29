57379f9b9cce6df197618e356ae935f2b18baae4
Erik Schilling <ablu.erikschilling@gmail.com>
2016-09-29 23:00:35 +0200

    Distribute input forms over the full width

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,21 +4,32 @@ import {MdIconRegistry} from '@angular/material';
 @Component({
     selector: 'my-app',
     providers: [MdIconRegistry],
+    styles: [`
+    #content {
+        padding: 20px;
+    }
+    .distribute {
+        display: flex;
+    }
+    
+    .spacer {
+        flex: 1 1 auto;
+    }
+`],
     template: `
     <md-toolbar color="primary">
         <md-icon class="md-24" style="padding: 0 14px;">menu</md-icon>
         <span>A SHARE implementation</span>
     </md-toolbar>
-    <div>
-    <div>
-        <datastorer></datastorer>
-    </div>
-    <div>
-        <stretch-factor-edit></stretch-factor-edit>
-    </div>
-    <div>
-    <storage-node-list></storage-node-list>
-    </div>
+    <div id="content">
+        <div class="distribute">
+            <datastorer></datastorer>
+            <span class="spacer"></span>
+            <stretch-factor-edit></stretch-factor-edit>
+        </div>
+        <div>
+            <storage-node-list></storage-node-list>
+        </div>
     </div>
     <add-storagenode></add-storagenode>
   `
